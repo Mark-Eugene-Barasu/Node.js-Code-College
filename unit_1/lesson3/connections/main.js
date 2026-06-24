de01d952d6e2a26e2e4aea539499b67204db140b
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-06-24 19:49:17 +0200

    done with lesson 3

---
+++ unit_1/lesson3/connections/main.js
@@ -0,0 +1,4 @@
+const cities = require("cities");
+
+let myCity = cities.zip_lookup("57370");
+console.log(myCity);
--- unit_1/lesson3/printMessages.js
+++ unit_1/lesson3/printMessages.js
@@ -1,6 +1,9 @@
+"use strict";
+
 //require messages module (we can access the array)
 const messageModule = require("./messages");
 
-messageModule.messages.forEach(m => 
-    console.log(m)
-);
+messageModule.messages.forEach(m => {
+    let i = messageModule.messages.indexOf(m) + 1;
+    console.log(i, m);
+});
